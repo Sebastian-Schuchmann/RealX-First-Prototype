20f5637f3c2df3133a7e6728d2f2ace23c2dd46e
Beatmeier <Robin-Bittlinger@web.de>
2019-05-30 17:59:13 +0200

    Zuweisung von Befehlen auf einzelnen "Töne"

--- Buttons/Midi.py
+++ Buttons/Midi.py
@@ -1,10 +1,20 @@
+import sys
 import rtmidi
+import threading
 
 midiin = rtmidi.RtMidiIn()
 
 def print_message(midi):
     if midi.isNoteOn():
         print('ON: ', midi.getMidiNoteName(midi.getNoteNumber()), midi.getVelocity())
+    elif midi.getMidiNoteName(midi.getNoteNumber()) == "G#0":
+        print('Pause')
+    elif midi.getMidiNoteName(midi.getNoteNumber()) == "C0":
+        print('Deep Work')
+    elif midi.getMidiNoteName(midi.getNoteNumber()) == "E-1":
+        print('Casual Work')
+    elif midi.getMidiNoteName(midi.getNoteNumber()) == "G#-2":
+        print('Freizeit')
     elif midi.isNoteOff():
         print('OFF:', midi.getMidiNoteName(midi.getNoteNumber()))
     elif midi.isController():
@@ -14,7 +24,7 @@ ports = range(midiin.getPortCount())
 if ports:
     for i in ports:
         print(midiin.getPortName(i))
-    print("Opening port 0!") 
+    print("Starting RealX") 
     midiin.openPort(0)
     while True:
         m = midiin.getMessage(250) # some timeout in ms
@@ -22,3 +32,6 @@ if ports:
             print_message(m)
 else:
     print('NO MIDI INPUT PORTS!')
+
+
+     
