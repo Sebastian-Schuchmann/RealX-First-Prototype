a0aff22a471ffe26d3145f1c5a28c8a569d1f8ac
Seppi <schuchmannsebastian@gmail.com>
2019-05-30 19:10:05 +0200

    First Progress on Lights

---
+++ Lights/Examples/example_async.py
@@ -0,0 +1,118 @@
+#!/usr/bin/env python3
+"""
+This is an example of how the pytradfri-library can be used async.
+
+To run the script, do the following:
+$ pip3 install pytradfri
+$ Download this file (example_async.py)
+$ python3 example_async.py <IP>
+
+Where <IP> is the address to your IKEA gateway. The first time
+running you will be asked to input the 'Security Code' found on
+the back of your IKEA gateway.
+"""
+
+# Hack to allow relative import above top level package
+import sys
+import os
+folder = os.path.dirname(os.path.abspath(__file__))  # noqa
+sys.path.insert(0, os.path.normpath("%s/.." % folder))  # noqa
+
+from pytradfri import Gateway
+from pytradfri.api.aiocoap_api import APIFactory
+from pytradfri.error import PytradfriError
+from pytradfri.util import load_json, save_json
+
+import asyncio
+import uuid
+import argparse
+
+CONFIG_FILE = 'tradfri_standalone_psk.conf'
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('host', metavar='IP', type=str,
+                    help='IP Address of your Tradfri gateway')
+parser.add_argument('-K', '--key', dest='key', required=False,
+                    help='Key found on your Tradfri gateway')
+args = parser.parse_args()
+
+
+if args.host not in load_json(CONFIG_FILE) and args.key is None:
+    print("Please provide the 'Security Code' on the back of your "
+          "Tradfri gateway:", end=" ")
+    key = input().strip()
+    if len(key) != 16:
+        raise PytradfriError("Invalid 'Security Code' provided.")
+    else:
+        args.key = key
+
+
+async def run():
+    # Assign configuration variables.
+    # The configuration check takes care they are present.
+    conf = load_json(CONFIG_FILE)
+
+    try:
+        identity = conf[args.host].get('identity')
+        psk = conf[args.host].get('key')
+        api_factory = APIFactory(host=args.host, psk_id=identity, psk=psk)
+    except KeyError:
+        identity = uuid.uuid4().hex
+        api_factory = APIFactory(host=args.host, psk_id=identity)
+
+        try:
+            psk = await api_factory.generate_psk(args.key)
+            print('Generated PSK: ', psk)
+
+            conf[args.host] = {'identity': identity,
+                               'key': psk}
+            save_json(CONFIG_FILE, conf)
+        except AttributeError:
+            raise PytradfriError("Please provide the 'Security Code' on the "
+                                 "back of your Tradfri gateway using the "
+                                 "-K flag.")
+
+    api = api_factory.request
+
+    gateway = Gateway()
+
+    devices_command = gateway.get_devices()
+    devices_commands = await api(devices_command)
+    devices = await api(devices_commands)
+
+    lights = [dev for dev in devices if dev.has_light_control]
+
+    # Print all lights
+    print(lights)
+
+    # Lights can be accessed by its index, so lights[1] is the second light
+    if lights:
+        light = lights[0]
+    else:
+        print("No lights found!")
+        light = None
+
+    if light:
+        # Example 4: Set the light level of the light
+        dim_command = light.light_control.set_dimmer(254)
+        await api(dim_command)
+
+        # Example 5: Change color of the light
+        # f5faf6 = cold | f1e0b5 = normal | efd275 = warm
+        color_command = light.light_control.set_hex_color('efd275')
+        await api(color_command)
+
+   # tasks_command = gateway.get_smart_tasks()
+    #tasks_commands = await api(tasks_command)
+    #tasks = await api(tasks_commands)
+
+
+    #print("Waiting for observation to end (2 mins)")
+   # print("Try altering any light in the app, and watch the events!")
+    #await asyncio.sleep(120)
+
+   # await api_factory.shutdown()
+
+
+asyncio.get_event_loop().run_until_complete(run())
