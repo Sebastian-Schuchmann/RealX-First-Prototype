f8db4bb8e821092a55ee80f527d912011e708be4
Beatmeier <Robin-Bittlinger@web.de>
2019-05-30 19:00:39 +0200

    Einbindung vom Spotify-Script. Belegung der Buttons mit verschiedenen Playlists. Lautstärke-Regler. Unterscheidung verschiedener Reglerwerte in ein Pausensystem.

--- Buttons/Midi.py
+++ Buttons/Midi.py
@@ -1,20 +1,41 @@
 import sys
 import rtmidi
 import threading
+from importlib.machinery import SourceFileLoader
+
+Spotify = SourceFileLoader("Spotify.py", "Spotify/Spotify.py").load_module()
+Spotify.initialize()
 
 midiin = rtmidi.RtMidiIn()
+spotifyUser = Spotify.initialize()
+pausentimer = 0
 
 def print_message(midi):
     if midi.isNoteOn():
         print('ON: ', midi.getMidiNoteName(midi.getNoteNumber()), midi.getVelocity())
     elif midi.getMidiNoteName(midi.getNoteNumber()) == "G#0":
         print('Pause')
+        Spotify.playPlaylistByName(spotifyUser, Spotify.BREAK)
     elif midi.getMidiNoteName(midi.getNoteNumber()) == "C0":
         print('Deep Work')
+        Spotify.playPlaylistByName(spotifyUser, Spotify.DEEPWORK)
     elif midi.getMidiNoteName(midi.getNoteNumber()) == "E-1":
         print('Casual Work')
+        Spotify.playPlaylistByName(spotifyUser, Spotify.CASUALWORK)
     elif midi.getMidiNoteName(midi.getNoteNumber()) == "G#-2":
         print('Freizeit')
+        Spotify.playPlaylistByName(spotifyUser, Spotify.FREIZEIT)
+    elif midi.getControllerNumber() == 7:
+        spotifyUser.volume(midi.getControllerValue())
+    elif midi.getControllerNumber() == 48:
+        pausentimer = midi.getControllerValue()
+        print('Timer auf:', pausentimer)
+        if pausentimer <= 5:
+            print('Micropause')
+        elif pausentimer <= 25:
+            print('Ritualpause')
+        elif pausentimer > 25:
+            print('Mittagspause')
     elif midi.isNoteOff():
         print('OFF:', midi.getMidiNoteName(midi.getNoteNumber()))
     elif midi.isController():
